54fa96fcd3a8b529c11d1c60b4ea27b34eb3d97d
Md Aminul <md.aminul.islam@staht.com>
2022-08-25 11:58:31 +0100

    ws added

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-const app = require('express')();
-const appWs = require('express-ws')(app);
+// Importing the required modules
+const WebSocketServer = require('ws');
 const PORT = process.env.PORT || 3000;
-app.ws('/echo', ws => {
-    ws.on('message', msg => {
-        console.log('Received: ', msg);
-        ws.send("REPLY : "+msg);
-    });
-});
+// Creating a new websocket server
+const wss = new WebSocketServer.Server({ port: PORT })
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+// Creating connection using websocket
+wss.on("connection", ws => {
+    console.log("new client connected");
+    // sending message
+    ws.on("message", data => {
+        console.log(`Client has sent us: ${data}`)
+    });
+
+    // handling what to do when clients disconnects from server
+    ws.on("close", () => {
+        console.log("the client has connected");
+    });
+    // handling client connection error
+    ws.onerror = function () {
+        console.log("Some Error occurred")
+    }
+});
+console.log("The WebSocket server is running on port "+PORT);
+
+// const app = require('express')();
+// const appWs = require('express-ws')(app);
+// const PORT = process.env.PORT || 3000;
+// app.ws('/echo', ws => {
+//     ws.on('message', msg => {
+//         console.log('Received: ', msg);
+//         ws.send("REPLY : "+msg);
+//     });
+// });
+//
+// app.listen(PORT, () => console.log(`Listening on ${PORT}`));
